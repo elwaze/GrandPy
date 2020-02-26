0a71fa5bc97a1ee9c605f3e66bf797f83fb27140
Elwaze <eloise.lamontagne@gmail.com>
2020-02-26 16:58:32 +0100

    env

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
+#! /usr/bin/env python
+import os
+
 GOOGLE_MAPS_URL = "https://maps.googleapis.com/maps/api/geocode/json?address="
-GOOGLE_KEY = "AIzaSyBmyCPrDf0hIaBEaHYvIIVfGj_tBwLCWsU"
+GOOGLE_KEY = os.environ.get('GOOGLE_API_KEY')
 WIKIPEDIA_URL = ""
