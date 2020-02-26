ec72e49f58edf1ebcd8800269e68e45efde94581
Elwaze <eloise.lamontagne@gmail.com>
2020-02-26 09:31:54 +0100

    TestParser OK

--- gpapp/gpmodules/parser.py
+++ gpapp/gpmodules/parser.py
@@ -1,1 +1,23 @@
 #! /usr/bin/env python
+""" Module parsing the user's question to get keywords to insert in requests."""
+
+import json
+
+
+class Parser:
+    """Class which returns the keywords."""
+
+    def __init__(self):
+        """Loads filters contained in the 'stop_words.json' file"""
+        with open('stop_words.json') as file:
+            stop_words = json.loads(file.read())
+        self.stop_words = stop_words["stop_words"]
+
+    def split(self, question):
+        """Splits the question into a list of words"""
+
+    def remove_stopwords(self, question):
+        """Removes the useless stopwords contained in the 'stop_words.json file"""
+
+    def get_key_words(self, question):
+        """Selects the useful information to be inserted in the requests"""
--- gpapp/tests/test_gpapp.py
+++ gpapp/tests/test_gpapp.py
@@ -1,32 +1,67 @@
 #! /usr/bin/env python
 
-from gpapp.gpmodules import parser, map_requestor, wiki_requestor
+from gpapp.gpmodules.parser import Parser
+# from gpapp.gpmodules.map_requestor import Requestor
+# from gpapp.gpmodules.wiki_requestor import Requestor
 
-# modele
-def mock_fonctionamocker():
-    # rq : peut etre def dans la fonction de test si n'est pas utilise apres
-    return "resultat attendu"
+# # modele
+# def mock_fonctionamocker():
+#     # rq : peut etre def dans la fonction de test si n'est pas utilise apres
+#     return "resultat attendu"
+#
+#
+# def test_nomfonction_cequejeteste(monkeypatch):
+#     # soit ...(contexte de test (setup))
+#     monkeypatch.setattr("gpapp.app.fonctionamocker", mock_fonctionamocker())
+#     # quand... (action)
+#     result = parser.nomfonction()
+#     # alors je constate que... (resultat)
+#     assert result == "resultat attendu"
+# # fin modele
 
 
-def test_nomfonction_cequejeteste(monkeypatch):
-    # soit ...(contexte de test (setup))
-    monkeypatch.setattr("gpapp.app.fonctionamocker", mock_fonctionamocker())
-    # quand... (action)
-    result = parser.nomfonction()
-    # alors je constate que... (resultat)
-    assert result == "resultat attendu"
-# fin modele
-
-
-class ParserTest:
+class TestParser:
     """Class for tests of functions contained in module parser.py"""
-    def test_parsing_(self):
-        # soit ...(contexte de test (setup))
-        question = ""
-        # quand... (action)
-        result = parser.get_key_words(question)
-        # alors je constate que... (resultat)
-        assert result == "resultat attendu"
+    def setup(self):
+        """Setup for the parser tests"""
+        self.parser = Parser()
+        self.questions = [
+            ("dis, grand-py, tu peux me donner l'adresse de la Tour Eiffel ?",
+             ["dis", "grand-py", "tu", "peux", "me", "donner", "l", "adresse", "de", "la", "tour", "eiffel"],
+             ["donner", "adresse", "tour", "eiffel"],
+             "tour eiffel"),
+            ("Salut pépé ! Où je peux trouver le parc de la tête d'or à Lyon ?",
+             ["salut", "pépé", "où", "je", "peux", "trouver", "le", "parc", "de", "la", "tête", "d", "or", "à", "Lyon"],
+             ["trouver", "parc", "tête", "or", "lyon"],
+             "parc tete or lyon")
+        ]
+
+    def test_split_question(self):
+        """Checks that the split function returns a list of words"""
+
+        # calling function
+        for question in self.questions:
+            result = self.parser.split(question[0])
+            # expected result
+            assert result == question[1]
+
+    def test_question_cleaned(self):
+        """Checks that remove_stopwords() returns a list of words not in the stopwords"""
+
+        # calling function
+        for question in self.questions:
+            result = self.parser.remove_stopwords(question[1])
+            # expected result
+            assert result == question[2]
+
+    def test_get_key_words(self):
+        """Checks that get_key_words() returns only the useful words"""
+
+        # calling function
+        for question in self.questions:
+            result = self.parser.get_key_words(question[2])
+            # expected result
+            assert result == question[3]
 
 
 class MapTest:
