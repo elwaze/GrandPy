633e7b17f79b91e49241ba7d64c0e84767d132c7
Elwaze <eloise.lamontagne@gmail.com>
2020-03-18 16:23:10 +0100

    wiki tests extract

--- config.py
+++ config.py
@@ -2,5 +2,5 @@
 import os
 
 GOOGLE_MAPS_URL = "https://maps.googleapis.com/maps/api/geocode/json?address="
-GOOGLE_KEY = os.environ.get('GOOGLE_API_KEY', "AIzaSyBmyCPrDf0hIaBEaHYvIIVfGj_tBwLCWsU")
+GOOGLE_KEY = os.environ.get('GOOGLE_API_KEY')
 WIKIPEDIA_URL = "https://fr.wikipedia.org/w/api.php?action=query&prop=extracts&format=json"
