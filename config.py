5c6f62f41e9824b1291e114b54a941058c4a7206
Elwaze <eloise.lamontagne@gmail.com>
2020-04-01 16:04:48 +0200

    worked on js map view

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # GOOGLE_JS_KEY = os.environ.get('GOOGLE_JS_KEY')
 GOOGLE_MAPS_URL = "https://maps.googleapis.com/maps/api/geocode/json?address="
-GOOGLE_KEY = os.environ.get('GOOGLE_API_KEY', 'AIzaSyD6M-j1zs4R1lcOWQnZvQ4T-vpeO6qz1Qc')
+GOOGLE_KEY = os.environ.get('GOOGLE_API_KEY')
 WIKIPEDIA_URL = "https://fr.wikipedia.org/w/api.php?action=query&prop=extracts&format=json"
 WIKILINK = "https://fr.wikipedia.org/wiki/"
 GOOGLE_MAPS_LINK = "https://maps.googleapis.com/maps/api/js?key=" + GOOGLE_KEY + "&callback=initMap"
